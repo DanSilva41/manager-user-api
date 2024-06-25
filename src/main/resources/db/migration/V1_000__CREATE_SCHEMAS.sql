55947ea3d72eb9ff39cd6e82115b2cde17913634
Danilo Silva <danilosilvap@outlook.com.br>
2024-06-25 20:59:31 -0300

    feat(persist-user): add sql scripts
    - main schemas
    - person table
    - user table

---
+++ src/main/resources/db/migration/V1_000__CREATE_SCHEMAS.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS backing
+    AUTHORIZATION "app-manager-user";
+
+CREATE SCHEMA IF NOT EXISTS security
+    AUTHORIZATION "app-manager-user";
---
+++ src/main/resources/db/migration/V1_001__CREATE_PERSON_TABLE.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS backing.person (
+    code            INT             NOT NULL,
+    first_name      VARCHAR(60)     NOT NULL,
+    last_name       VARCHAR(60)     NOT NULL,
+    email           VARCHAR(100)    NOT NULL,
+    created_at      TIMESTAMPTZ     NOT NULL,
+    updated_at      TIMESTAMPTZ     NOT NULL,
+    CONSTRAINT pk_person PRIMARY KEY (code),
+    CONSTRAINT un_person_email UNIQUE (email)
+);
