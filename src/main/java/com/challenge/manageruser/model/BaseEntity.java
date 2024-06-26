340067e686bd57da065f0c478eb98254b50e0a96
Danilo Silva <danilosilvap@outlook.com.br>
2024-06-25 21:01:19 -0300

    feat(persist-user): add base entity

---
+++ src/main/java/com/challenge/manageruser/model/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.challenge.manageruser.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+/**
+ * Base abstract class for entities that will contain definitions for created at and updated at.
+ */
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @CreationTimestamp
+    private Instant updatedAt;
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+}
