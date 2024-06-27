b60c20b60ae61dc1b38620c7a74740dc9ae915a3
Danilo Silva <danilosilvap@outlook.com.br>
2024-06-27 16:21:54 -0300

    fix(persist-department): adjust migration V1_103__CREATE_DEPARTMENT_TABLE.sql

--- src/main/resources/db/migration/V1_103__CREATE_DEPARTMENT_TABLE.sql
+++ src/main/resources/db/migration/V1_103__CREATE_DEPARTMENT_TABLE.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS backing."department" (
-    name        VARCHAR(20) NOT NULL,
-    description VARCHAR(60) NOT NULL,
+    name        VARCHAR(60) NOT NULL,
+    description VARCHAR(100) NOT NULL,
     created_at  TIMESTAMPTZ NOT NULL,
     updated_at  TIMESTAMPTZ NOT NULL,
     CONSTRAINT pk_user PRIMARY KEY (name)
